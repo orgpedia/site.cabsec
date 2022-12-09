27ed84d766ac9fd0147ef899d299d581a56956fb
mukundesh <mukundesh@outlook.com>
2022-12-09 12:56:48 +0530

    minifyHTML_

--- flow/minifyHTML_/run_all.sh
+++ flow/minifyHTML_/run_all.sh
@@ -1,3 +1,12 @@
-for f in `ls input/*.html |xargs  basename`; do 
-	sh src/html_minify input/$f > output/$f; 
-done
+LANG="as bn brx doi gu hi kn ks gom mai mni ml mr ne or pa sa sat sd ta te ur en";
+for l in `echo $LANG`; do
+echo "starting $l";
+for f in `ls input/$l/*.html |xargs  basename `; do 
+	sh src/html_minify input/$l/$f > output/$l/$f; 
+	#echo "$l/$f";
+done;
+echo "done $l";
+done;
+
+sh src/html_minify input/index.html > output/index.html;
+sh src/html_minify input/disclaimer.html > output/disclaimer.html;
