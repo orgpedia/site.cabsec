2616a47794a4b41eabdf56d7da142d0064564a77
mukundesh <mukundesh@outlook.com>
2023-02-21 15:13:33 +0530

    added images and link creation code

--- flow/genSite_/src/genSite.py
+++ flow/genSite_/src/genSite.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -13,13 +14,23 @@ if __name__ == '__main__':
 
     viz = docint.load('src/genSite.yml')
 
+    if not (output_path / 'cors-server.py').exists():
+        src_path = Path('..') / Path('src') / 'cors-server.py'
+        dst_path = output_path / 'cors-server.py'
+        if not dst_path.exists():
+            os.symlink(src_path, dst_path)
+
+    for (src_dir, dst_dir) in [('js', 'j'), ('css', 'c'), ('images', 'i')]:
+        src_path = Path('..') / Path('src') / src_dir
+        dst_path = output_path / dst_dir
+        if not dst_path.exists():
+            os.symlink(src_path, dst_path)
+
     if input_path.is_dir():
         assert output_path.is_dir()
         input_files = input_path.glob('*.order.json')
 
         docs = viz.pipe_all(input_files)
-        
+
         for doc in docs:
             print(f'{doc.pdf_name}')
-
-        
